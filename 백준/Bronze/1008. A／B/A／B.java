35db45c13a0c05c8cf52ff159254b232d9e9965f
brightk123 <brightvsun@gmail.com>
2025-07-26 22:27:27 +0900

    [Bronze V] Title: A/B, Time: 172 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextInt();
+		double b = sc.nextInt();
+		
+		double ans = a/b;
+		System.out.println(ans);
+		
+	}
+}
